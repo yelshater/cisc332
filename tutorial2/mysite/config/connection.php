af8d838bd5b07b7fd677a41f5dff383400513888
yelshater <y.z.elshater@gmail.com>
2017-03-02 15:01:12 -0500

    committing tutorial2 final version

--- tutorial2/mysite/config/connection.php
+++ tutorial2/mysite/config/connection.php
@@ -1,7 +1,7 @@
 <?php
 // used to connect to the database
 $host = "localhost";
-$db_name = "cisc332_2";
+$db_name = "cisc332db";
 $username = "cisc332";
 $password = "cisc332password";
 
@@ -13,14 +13,4 @@ try {
 catch(Exception $exception){
     echo "Connection error: " . $exception->getMessage();
 }
-/*
- $con = new mysqli($host,$username,$password, $db_name);
- //$con = mysqli_connect($host,$username,$password, $db_name);
- // Check connection
- if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  die();
-  }
-   */
 ?>
